97065fe4514848cebb883a65b53c2c8982e4f084
adrdigital-bnb <adrdigital@gmail.com>
2026-02-08 20:37:19 -0300

    compile trece

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,15 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// 👇 AGREGAMOS LAS CLAVES AQUÍ DIRECTAMENTE
-// Reemplaza los textos entre comillas con tus claves reales de .env.local
-export default clerkMiddleware(undefined, {
-  publishableKey: "pk_test_cHJvcGVyLXN0dWQtMjQuY2xlcmsuYWNjb3VudHMuZGV2JA",
+// 👇 CAMBIO: En vez de 'undefined', ponemos una función vacía '() => {}'
+// IMPORTANTE: Asegúrate de que las claves "pk_test" y "sk_test" sean las REALES
+export default clerkMiddleware(() => {}, {
+  publishableKey: "pk_test_cHJvcGVyLXN0dWQtMjQuY2xlcmsuYWNjb3VudHMuZGV2JA", 
   secretKey: "sk_test_rKNwn5jrtuafxDhpgVr5dJ2sBJF5RQQQag8AiNSXa5"
 });
 
 export const config = {
   matcher: [
-    // Protege todas las rutas excepto los estáticos
+    // Protege todas las rutas excepto los archivos estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Siempre corre para rutas API
     '/(api|trpc)(.*)',
